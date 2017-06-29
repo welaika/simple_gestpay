186f789b5f363b4410d30725c0287360fed9303c
Fabrizio Monti <fabrizio.monti@welaika.com>
2017-06-29 15:37:27 +0200

    Require all gem files

--- lib/simple_gestpay.rb
+++ lib/simple_gestpay.rb
@@ -11,6 +11,15 @@ require 'simple_gestpay/errors'
 require 'simple_gestpay/currencies'
 require 'simple_gestpay/languages'
 require 'simple_gestpay/configuration'
+require 'simple_gestpay/order'
+require 'simple_gestpay/order_decorator'
+require 'simple_gestpay/soap_client'
+require 'simple_gestpay/ws_crypt_decrypt_soap_client'
+require 'simple_gestpay/ws_crypt'
+require 'simple_gestpay/ws_decrypt'
+require 'simple_gestpay/encrypt_result'
+require 'simple_gestpay/decrypt_result'
+require 'simple_gestpay/payment_page'
 
 module SimpleGestpay
 end
