4cc65116f4d0af9e9e1895681e92a376855f72fd
Fabrizio Monti <fabrizio.monti@welaika.com>
2017-07-18 10:22:09 +0200

    Use strings for environment keys

--- lib/simple_gestpay/payment_page.rb
+++ lib/simple_gestpay/payment_page.rb
@@ -3,8 +3,10 @@
 module SimpleGestpay
   class PaymentPage
     URL_FORMAT = {
-      test: 'https://testecomm.sella.it/pagam/pagam.aspx?a=%{shop_login}&b=%{encrypted_string}',
-      production: 'https://ecomm.sella.it/pagam/pagam.aspx?a=%{shop_login}&b=%{encrypted_string}'
+      'test' =>
+        'https://testecomm.sella.it/pagam/pagam.aspx?a=%{shop_login}&b=%{encrypted_string}',
+      'production' =>
+        'https://ecomm.sella.it/pagam/pagam.aspx?a=%{shop_login}&b=%{encrypted_string}'
     }.freeze
 
     attr_reader :shop_login, :environment, :encrypted_string
@@ -18,7 +20,8 @@ module SimpleGestpay
     end
 
     def url
-      format(URL_FORMAT[environment], shop_login: shop_login, encrypted_string: encrypted_string)
+      format(URL_FORMAT.fetch(environment),
+             shop_login: shop_login, encrypted_string: encrypted_string)
     end
   end
 end
